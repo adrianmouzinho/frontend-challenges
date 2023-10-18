cfe417d987899a833e58fc901cd098fa9d4a4c56
Adrian Mouzinho <adrianmouzinhopro@gmail.com>
2023-10-18 12:26:26 -0300

    feat: add form control and validation

--- smartfit-challenge/src/app/components/form/form.component.ts
+++ smartfit-challenge/src/app/components/form/form.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.scss']
 })
-export class FormComponent {
+export class FormComponent implements OnInit {
+
+  unitsFound = [];
+  formGroup!: FormGroup;
+  submitted = false;
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.formGroup = this.formBuilder.group({
+      timeOfDay: ['', [Validators.required]],
+      showClosedUnits: false
+    })
+  }
+
+  onSubmit() {
+    this.submitted = true;
+
+    if (this.formGroup.invalid) {
+      return;
+    }
+
+    console.log(this.formGroup.value)
+  }
+
+  onClear() {
+    this.submitted = false;
+    this.formGroup.reset();
+  }
 
 }
